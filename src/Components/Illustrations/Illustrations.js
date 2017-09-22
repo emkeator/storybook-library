7280bf067e082e63fc225194dfe8bb0bf5033ac4
emkeator <emkeator@gmail.com>
2017-09-22 16:45:12 -0600

    updates

--- src/Components/Illustrations/Illustrations.js
+++ src/Components/Illustrations/Illustrations.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import mountains from './../../images/mountain_PNG30.png';
-import arches1 from './../../images/arches1.png';
-import arches2 from './../../images/arches2.png';
-import bryce1 from './../../images/bryce1.png';
-import bryce2 from './../../images/bryce2.png';
-import skiing1 from './../../images/skiing1.png';
-import skiing2 from './../../images/skiing2.png';
-import yosemite1 from './../../images/yosemite1.png';
-import yosemite2 from './../../images/yosemite2.png';
-import guardsman from './../../images/guardsman.png';
+import arches1 from './../../images/arches1_long.png';
+import arches2 from './../../images/arches2_long.png';
+import bryce1 from './../../images/bryce1_long.png';
+import bryce2 from './../../images/bryce2_long.png';
+import skiing1 from './../../images/skiing1_long.png';
+import skiing2 from './../../images/skiing2_long.png';
+import yosemite1 from './../../images/yosemite1_long.png';
+import yosemite2 from './../../images/yosemite2_long.png';
+import guardsman from './../../images/guardsman_long.png';
 import sunset from './../../images/sunset.jpg';
 import TweenMax from 'gsap';
 import $ from 'jquery';
@@ -42,14 +42,19 @@ export default class Illustrations extends Component {
 
     forward() {
         let i = this.state.currentPage
-        TweenMax.to($(`.page${i}`), 2, {scale: '2', bottom: '-1400px', delay: '0',ease: TweenMax.Power1.easeIn});
-        TweenMax.to($(`.page${i}second`), 1, {scaleX: '2', scaleY:'-2', bottom: '-1400px', delay: '0',ease: TweenMax.Power1.easeIn});                
+        TweenMax.to($(`.page${i}`), 4, {scale: '10', bottom: '-2000px', delay: '0.5', ease: TweenMax.Power1.easeIn});
+        TweenMax.to($(`.page${i}second`), 3, {scaleX: '10', scaleY:'-10', bottom: '-2000px', delay: '0.5',ease: TweenMax.Power1.easeIn});
+        TweenMax.to($(`.page${i}`), 3, {opacity: '0', delay: '1', ease: TweenMax.Power1.easeIn});
+        TweenMax.to($(`.page${i}second`), 3, {opacity: '0', delay: '1',ease: TweenMax.Power1.easeIn});              
         
-        TweenMax.to($(`.page${i + 1}`), 2, {bottom: '0px', opacity: '1', delay: '-0.5',ease: TweenMax.Power1.easeOut});
-        TweenMax.to($(`.page${i + 1}second`), 3, {bottom: '20px', opacity: '1', delay: '-0.5',ease: TweenMax.Power1.easeOut});
+        TweenMax.to($(`.page${i + 1}`), 3, {scale: '1', bottom: '0px', delay: '0.1', ease: TweenMax.Power1.easeOut});
+        TweenMax.to($(`.page${i + 1}second`), 2, {scaleX: '1', scaleY:'-1', bottom: '20px', delay: '0.1',ease: TweenMax.Power1.easeOut});
+
+        TweenMax.to($(`.page${i + 2}`), 3, {scale: '0.55', bottom: '-125px', delay: '0.1', ease: TweenMax.Power1.easeOut});
+        TweenMax.to($(`.page${i + 2}second`), 2, {scaleX: '0.55', scaleY:'-0.55', bottom: '-105px', delay: '0.1',ease: TweenMax.Power1.easeOut});
         
         if(i === this.state.numPages - 1){
-            TweenMax.to($('.background'), 2, {opacity: '1', delay: '0',ease: TweenMax.Power1.easeIn})
+            TweenMax.to($('.background'), 3, {opacity: '1', delay: '0',ease: TweenMax.Power1.easeIn})
         }
 
         if(i < this.state.numPages) i += 1;
@@ -69,11 +74,14 @@ export default class Illustrations extends Component {
             TweenMax.to($('.background'), 2, {opacity: 0, delay: '0',ease: TweenMax.Power1.easeIn})
         }
 
-        TweenMax.to($(`.page${x}`), 2, {scale: '1', bottom: '0px', delay: '-1',ease: TweenMax.Power1.easeInOut});
-        TweenMax.to($(`.page${x}second`), 3, {scaleX: '1', scaleY:'-1', bottom: '20px', delay: '-1',ease: TweenMax.Power1.easeInOut});                
+        TweenMax.to($(`.page${x}`), 3, {scale: '1', bottom: '0px', opacity: '1', delay: '-1',ease: TweenMax.Power1.easeInOut});
+        TweenMax.to($(`.page${x}second`), 4, {scaleX: '1', scaleY:'-1', bottom: '20px', opacity: '1', delay: '-1',ease: TweenMax.Power1.easeInOut});                
         
-        TweenMax.to($(`.page${x + 1}`), 4, {bottom: '-1400px', opacity: '0', delay: '-0.5', ease: TweenMax.Power1.easeInOut});
-        TweenMax.to($(`.page${x + 1}second`), 3, {bottom: '-1400px', opacity: '0', delay: '-0.5', ease: TweenMax.Power1.easeInOut});
+        TweenMax.to($(`.page${x + 1}`), 4, {scale: '0.55', bottom: '-125px', delay: '-0.5', ease: TweenMax.Power1.easeInOut});
+        TweenMax.to($(`.page${x + 1}second`), 3, {scaleX: '0.55', scaleY:'-0.55', bottom: '-105px', delay: '-0.5', ease: TweenMax.Power1.easeInOut});
+
+        TweenMax.to($(`.page${x + 2}`), 4, {scale: '0.5', bottom: '-142px', delay: '-0.5', ease: TweenMax.Power1.easeInOut});
+        TweenMax.to($(`.page${x + 2}second`), 3, {scaleX: '0.5', scaleY:'-0.5', bottom: '-142px', delay: '-0.5', ease: TweenMax.Power1.easeInOut});
     }
     renderIllus(id) {
         switch (+id) {
